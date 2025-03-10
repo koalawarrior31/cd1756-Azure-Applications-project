d738bcee96c5bac0cf3b3132dc9e1722b4431856
Valencia <martinzechariah.valencia@chevron.com>
2025-03-10 16:22:26 +0800

    Updated user login logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -62,7 +62,6 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        app.logger.info(f'Login attempt successful for user {user}')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -74,6 +73,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
+            app.logger.info(f'Login attempt successful for user {user}')
             next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
