1f8b358bc0492a49b4f8c71468192e6cff8a533e
Valencia <martinzechariah.valencia@chevron.com>
2025-03-10 13:02:09 +0800

    Removed secret keys in config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cms-article-sqldb'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'WWav4ZozIZMINw49KaIqgR/oCjcE2Q2CPYewRmd7CR6HY2smIv0AMJCIre9gz8E0Q8RpgmujB7q/+AStW4Ws8Q=='
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or ''
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms-article-sqlserver.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms-article-sqldb'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'zbzf'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Mistersnake31!'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or ''
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
